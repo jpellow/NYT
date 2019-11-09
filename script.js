8ec1c593ebfcda2b5412db92e1e44ebf346c4e40
Jeremy Pellow <jeremy.pellow@gmail.com>
2019-11-09 13:31:48 -0600

    error checking for years

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
     $("#button").on("click", function() {
-        var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=20120101&end_date=20120101&api-key="
+        var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=" + startYear +"&end_date=20120101&api-key="
         var term = $("#term").val();
         //console.log(term);
         var records = $("#records").val();
         //console.log(records);
         var startYear = $("#startYear").val();
-        console.log(startYear);
         var endYear = $("#endYear").val();
+
+        console.log(startYear);
+        if (isNaN(startYear)){
+            alert("Please enter a start year");
+        }
+        if (isNaN(endYear)){
+            alert("Please enter an end year");
+        }
+
         console.log(endYear);
-    })
+    });
 
 });
